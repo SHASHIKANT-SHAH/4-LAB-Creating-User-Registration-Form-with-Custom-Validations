357af1af3c533e269aaf729099537c64d27c41f0
shashikant <shashikantshah159@gmail.com>
2022-12-10 21:30:22 +0530

    SignUpModel Update

--- UserRegistrationFormwithValidations/Models/SignupViewModel.cs
+++ UserRegistrationFormwithValidations/Models/SignupViewModel.cs
@@ -14,8 +14,9 @@ namespace UserRegistrationFormwithValidations.Models
         public string Email { get; set; }
 
         [Required(ErrorMessage ="Password Required")]
-        [MinLength(8,ErrorMessage ="Password must be at least 8 character long")]
-        [RegularExpression("([a-zA-Z]{1,})([@$!%*#?&]{1,})([0-9]{1,})",ErrorMessage = "1 Uppercase, 1 Number, 1 Special Char, and 1 Lowercase Char Required")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Password required (1 Uppercase, 1 Number, 1 Special Char and 1 Lowercase Char) with Min Length 8 Chars")]
+        //[MinLength(8,ErrorMessage ="Password must be at least 8 character long")]
+        //[RegularExpression("([a-zA-Z]{1,})([@$!%*#?&]{1,})([0-9]{1,})",ErrorMessage = "1 Uppercase, 1 Number, 1 Special Char, and 1 Lowercase Char Required")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm-Password Required")]
